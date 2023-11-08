fec63a909b3f3b3efac9635513adb63232939623
elverpython <elvermahmudov@gmail.com>
2023-11-08 16:59:38 +0600

    added homepage and install bootstrap5

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.views import View
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'core/homepage.html')
--- lovebeauty/settings.py
+++ lovebeauty/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_bootstrap5',
+
     'core',
 ]
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'lovebeauty.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
